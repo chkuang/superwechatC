92553dbecf26dc46dfdd19c10ff0b257dbaff74b
chenkuang <970744995@qq.com>
2016-07-20 11:06:47 +0800

    在本地数据库添加user表

--- src/cn/ucai/superwechat/db/DbOpenHelper.java
+++ src/cn/ucai/superwechat/db/DbOpenHelper.java
@@ -46,7 +46,16 @@ public class DbOpenHelper extends SQLiteOpenHelper{
 			+ UserDao.ROBOT_COLUMN_NAME_ID + " TEXT PRIMARY KEY, "
 			+ UserDao.ROBOT_COLUMN_NAME_NICK + " TEXT, "
 			+ UserDao.ROBOT_COLUMN_NAME_AVATAR + " TEXT);";
-			
+
+	private static final String SUPERWECHAT_USER_TABLE_CREATE = "CREATE TABLE "
+			+ UserDao.USER_TABLE_NAME + " ("
+			+ UserDao.USER_COLUMN_NAME_ID + " TEXT PRIMARY KEY, "
+			+ UserDao.USER_COLUMN_NAME_NICK + " TEXT, "
+			+ UserDao.USER_COLUMN_NAME_AVATAR+ " TEXT"
+			+ UserDao.USER_COLUMN_AVATAR_TYPE + " TEXT PRIMARY KEY, "
+			+ UserDao.USER_COLUMN_AVATAR_PATH + " TEXT, "
+			+ UserDao.USER_COLUMN_AVATAR_LAST_UPDATE_TIME + " TEXT);";
+
 	private static final String CREATE_PREF_TABLE = "CREATE TABLE "
             + UserDao.PREF_TABLE_NAME + " ("
             + UserDao.COLUMN_NAME_DISABLED_GROUPS + " TEXT, "
@@ -73,7 +82,7 @@ public class DbOpenHelper extends SQLiteOpenHelper{
 		db.execSQL(INIVTE_MESSAGE_TABLE_CREATE);
 		db.execSQL(CREATE_PREF_TABLE);
 		db.execSQL(ROBOT_TABLE_CREATE);
-		
+		db.execSQL(SUPERWECHAT_USER_TABLE_CREATE);
 	}
 
 	@Override
