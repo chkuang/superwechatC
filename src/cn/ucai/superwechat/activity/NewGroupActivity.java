65b634f182008e2ea2a2116ca6b586c8fa0d428e
chenkuang <970744995@qq.com>
2016-07-26 19:00:39 +0800

    创建群组病添加群组成员

--- src/cn/ucai/superwechat/activity/NewGroupActivity.java
+++ src/cn/ucai/superwechat/activity/NewGroupActivity.java
@@ -163,7 +163,7 @@ public class NewGroupActivity extends BaseActivity {
 		}).start();
 	}
 
-	private void createAppGroup(String groupId, String groupName, String desc, String[] members) {
+	private void createAppGroup(final String groupId, String groupName, String desc, final String[] members) {
 		boolean isPublic = checkBox.isChecked();
 		boolean invites = !isPublic;
 		File file = new File(OnSetAvatarListener.getAvatarPath(NewGroupActivity.this,I.AVATAR_TYPE_GROUP_PATH),
@@ -184,15 +184,20 @@ public class NewGroupActivity extends BaseActivity {
 					public void onSuccess(String s) {
 						Log.e(TAG,"s = "+s);
 						Result result = Utils.getResultFromJson(s, GroupAvatar.class);
+						GroupAvatar groupAvatar = (GroupAvatar) result.getRetData();
 						Log.e(TAG,"result = "+result);
 						if (result!=null && result.isRetMsg()){
-							runOnUiThread(new Runnable() {
-								public void run() {
-									progressDialog.dismiss();
-									setResult(RESULT_OK);
-									finish();
-								}
-							});
+							if(members!=null && members.length>0){
+								addGroupMemebers(groupId,members);
+							}else {
+								runOnUiThread(new Runnable() {
+									public void run() {
+										progressDialog.dismiss();
+										setResult(RESULT_OK);
+										finish();
+									}
+								});
+							}
 						}
 					}
 
@@ -204,6 +209,51 @@ public class NewGroupActivity extends BaseActivity {
 					}
 				});
 
+	}
+
+	private void addGroupMemebers(String hxid, String[] members) {
+		Log.e(TAG,"members = "+members);
+		Log.e(TAG,"members = "+members.toString());
+		String memberArr = "";
+		for (String m : members){
+			memberArr +=m+",";
+		}
+		memberArr = memberArr.substring(0,memberArr.length()-1);
+		Log.e(TAG,"memberArr = "+memberArr);
+		final OkHttpUtils2<String> utils = new OkHttpUtils2<String>();
+		utils.setRequestUrl(I.REQUEST_ADD_GROUP_MEMBER)
+				.addParam(I.Member.GROUP_HX_ID,hxid)
+				.addParam(I.Member.USER_NAME,memberArr)
+				.targetClass(String.class)
+				.execute(new OkHttpUtils2.OnCompleteListener<String>() {
+					@Override
+					public void onSuccess(String s) {
+						Log.e(TAG,"s = "+s);
+						Result result = Utils.getResultFromJson(s, GroupAvatar.class);
+						GroupAvatar groupAvatar = (GroupAvatar) result.getRetData();
+						Log.e(TAG,"result = "+result);
+						if(result != null && result.isRetMsg()){
+							runOnUiThread(new Runnable() {
+								public void run() {
+									progressDialog.dismiss();
+									setResult(RESULT_OK);
+									finish();
+								}
+							});
+
+						}else{
+							progressDialog.dismiss();
+							Toast.makeText(NewGroupActivity.this, st2 ,Toast.LENGTH_SHORT).show();
+						}
+					}
+
+					@Override
+					public void onError(String error) {
+						Log.e(TAG,"error = "+error);
+						progressDialog.dismiss();
+						Toast.makeText(NewGroupActivity.this, st2 + error,Toast.LENGTH_SHORT).show();
+					}
+				});
 	}
 
 	private void setProgressDialog() {
