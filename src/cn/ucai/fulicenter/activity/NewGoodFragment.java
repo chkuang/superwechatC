e5f9f07e6c3d35e3296597054ed2f962b329249f
chenkuang <970744995@qq.com>
2016-08-02 18:13:20 +0800

    防止下拉刷新的bug

--- src/cn/ucai/fulicenter/activity/NewGoodFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -83,6 +83,10 @@ public class NewGoodFragment extends Fragment {
                 int l = mGridLayoutManager.findLastVisibleItemPosition();
                 Log.e(TAG,"f="+f+","+"l="+l);
                 lastItemPosition = mGridLayoutManager.findLastVisibleItemPosition();
+                mSwipeRefreshLayout.setEnabled(mGridLayoutManager.findLastVisibleItemPosition()==0);
+                if (f==-1 || l == -1){
+                    lastItemPosition = mAdapter.getItemCount()-1;
+                }
             }
         });
     }
