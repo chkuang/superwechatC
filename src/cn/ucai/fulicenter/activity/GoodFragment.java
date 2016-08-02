fcc688fc2087259974ebfe0862366deb8e612b30
chenkuang <970744995@qq.com>
2016-08-02 10:43:09 +0800

    新品fragment的初始化布局

--- src/cn/ucai/fulicenter/activity/GoodFragment.java
+++ src/cn/ucai/fulicenter/activity/GoodFragment.java
@@ -3,28 +3,58 @@ package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.adapter.GoodAdapter;
+import cn.ucai.fulicenter.bean.NewGoodBean;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GoodFragment extends Fragment {
+    FuliCenterMainActivity mContext;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
-
-    public GoodFragment() {
-        // Required empty public constructor
-    }
-
-
+    RecyclerView mRecyclerView;
+    GridLayoutManager mGridLayoutManager;
+    GoodAdapter mAdapter;
+    List<NewGoodBean> mGoodList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_good, container, false);
+        mContext = (FuliCenterMainActivity) getContext();
+        View layout = View.inflate(mContext,R.layout.fragment_new_good,null);
+        mGoodList = new ArrayList<NewGoodBean>();
+        initView(layout);
+        return layout;
+    }
+    private void initView(View layout){
+        mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.srl_new_good);
+        mSwipeRefreshLayout.setColorSchemeColors(
+                R.color.google_blue,
+                R.color.google_yellow,
+                R.color.google_red,
+                R.color.google_green
+        );
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.rv_new_good);
+        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mGridLayoutManager.setOrientation(LinearLayout.VERTICAL);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mAdapter = new GoodAdapter(mContext,mGoodList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
