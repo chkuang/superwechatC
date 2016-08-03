049e4afd4a8db0e5e4d2cec9690ac9a4839162a1
chenkuang <970744995@qq.com>
2016-08-03 12:24:45 +0800

    初始化商品详情布局文件

---
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -0,0 +1,63 @@
+package cn.ucai.fulicenter.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import cn.ucai.fulicenter.D;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.view.FlowIndicator;
+import cn.ucai.fulicenter.view.SlideAutoLoopView;
+
+public class GoodDetailsActivity extends Activity {
+    private final static String TAG = GoodDetailsActivity.class.getSimpleName();
+    ImageView ivShare;
+    ImageView ivCollect;
+    ImageView ivCart;
+    TextView tvCartCount;
+
+    TextView tvGoodEnglishName;
+    TextView tvGoodName;
+    TextView tvGoodPriceCurrent;
+    TextView tvGoodPriceShop;
+
+    SlideAutoLoopView mSlideAutoLoopView;
+    FlowIndicator mFlowIndicator;
+    WebView mvGoodBrief;
+    int mGoodId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_details);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        mGoodId = getIntent().getIntExtra(D.GoodDetails.KEY_GOODS_ID,0);
+        Log.e(TAG,"mGoodId = "+mGoodId);
+    }
+
+    private void initView() {
+        ivShare = (ImageView) findViewById(R.id.iv_good_share);
+        ivCollect = (ImageView) findViewById(R.id.iv_good_collect);
+        ivCart = (ImageView) findViewById(R.id.iv_good_cart);
+        tvCartCount = (TextView) findViewById(R.id.tv_cart_count);
+        tvGoodEnglishName = (TextView) findViewById(R.id.tv_good_name_english);
+        tvGoodName = (TextView) findViewById(R.id.tv_good_name);
+        tvGoodPriceCurrent = (TextView) findViewById(R.id.tv_good_price_current);
+        tvGoodPriceShop = (TextView) findViewById(R.id.tv_good_price_shop);
+        mSlideAutoLoopView = (SlideAutoLoopView) findViewById(R.id.salv);
+        mFlowIndicator = (FlowIndicator) findViewById(R.id.indicator);
+        mvGoodBrief = (WebView) findViewById(R.id.wv_good_brief);
+        WebSettings settings = mvGoodBrief.getSettings();
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setBuiltInZoomControls(true);
+    }
+}
--- src/cn/ucai/fulicenter/adapter/GoodAdapter.java
+++ src/cn/ucai/fulicenter/adapter/GoodAdapter.java
@@ -1,6 +1,7 @@
 package cn.ucai.fulicenter.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,8 +17,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.activity.GoodDetailsActivity;
 import cn.ucai.fulicenter.bean.NewGoodBean;
 import cn.ucai.fulicenter.utils.ImageUtils;
 import cn.ucai.fulicenter.view.FooterViewHolder;
@@ -72,10 +75,17 @@ public class GoodAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if (holder instanceof GoodViewHolder){
             mGoodViewHolder = (GoodViewHolder) holder;
-            NewGoodBean good = mGoodList.get(position);
+            final NewGoodBean good = mGoodList.get(position);
             ImageUtils.setGoodThumb(mContext,mGoodViewHolder.ivGoodThumb,good.getGoodsThumb());
             mGoodViewHolder.tvGoodName.setText(good.getGoodsName());
             mGoodViewHolder.tvGoodPrice.setText(good.getCurrencyPrice());
+            mGoodViewHolder.layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mContext.startActivity(new Intent(mContext, GoodDetailsActivity.class)
+                            .putExtra(D.GoodDetails.KEY_GOODS_ID,good.getGoodsId()));
+                }
+            });
         }
         if (holder instanceof FooterViewHolder){
             mFooterViewHolder = (FooterViewHolder) holder;
@@ -114,13 +124,13 @@ public class GoodAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     }
 
     class GoodViewHolder extends RecyclerView.ViewHolder{
-        LinearLayout layoutGood;
+        LinearLayout layout;
         ImageView ivGoodThumb;
         TextView tvGoodName;
         TextView tvGoodPrice;
         public GoodViewHolder(View itemView) {
             super(itemView);
-            layoutGood = (LinearLayout) itemView.findViewById(R.id.layout_good);
+            layout= (LinearLayout) itemView.findViewById(R.id.layout_good);
             ivGoodThumb = (ImageView) itemView.findViewById(R.id.iv_good_thumb);
             tvGoodName = (TextView) itemView.findViewById(R.id.tv_good_name);
             tvGoodPrice = (TextView) itemView.findViewById(R.id.tv_good_price);
